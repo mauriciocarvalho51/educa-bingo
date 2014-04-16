ee73a5a5af75582701e491c532bc4bd496141574
Bruno <brunolpw@outlook.com>
2014-04-16 09:31:03 -0300

    Sorteio de valores randomicos

---
+++ indexRand.php
@@ -0,0 +1,32 @@
+﻿<?php
+	function sorteia(){
+	
+		$min = 1;
+		$max = 26;
+		$cont = 0;
+		$flag = true;
+		$arr = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		
+		do{
+			$flag = false;
+			$rand = Rand($min, $max);
+			
+			foreach($arr as $valor){			
+				if($valor == $rand){ $flag = true; break; }
+				}
+				
+				if($flag == false){
+					$arr[$cont] = $rand;
+					$cont++;
+					}
+			}while($cont < 26);
+			
+			
+		print "<hr>";
+		for($i = 0; $i < $max; $i++){
+			print '<img src="img/'.$arr[$i].'.png">';
+			}
+	}
+	
+	sorteia();
+?>
